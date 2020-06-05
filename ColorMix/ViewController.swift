46cab6b34704ec1af1587536d60f6997f274d420
Carter Smith <carters3@illinois.edu>
2020-06-05 14:16:04 -0400

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Carter Smith on 6/5/20.
+//  Copyright © 2020 Carter Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
